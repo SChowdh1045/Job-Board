38b079c5072b4f74bcd0b0fee2e6cb5d2b6a1895
Salman Chowdhury <schowdhury3262@gmail.com>
2024-01-15 00:32:04 -0500

    Finished sidebar filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,70 @@
-import JobListItem from "@/components/JobListItem";
-import prisma from "@/lib/prisma";
 import JobFilterSidebar from "@/components/JobFilterSidebar";
+import JobResults from "@/components/JobResults";
+import H1 from "@/components/ui/h1";
+import { JobFilterType } from "@/lib/validations";
+import { Metadata } from "next";
 
-export default async function Home() {
-  const jobsList = await prisma.job.findMany({
-    where: {approved: true},
-    orderBy: {createdAt: 'desc'}
-  })
+// 'searchParams' is a special object property that Next.js injects into the page component from the URL query string.
+type UrlProps = {
+  searchParams: {
+    q?: string;
+    type?: string;
+    location?: string;
+    remote?: string;
+  };
+};
+
+function getTitle({ q, type, location, remote }: JobFilterType) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
+
+  const titleSuffix = location ? `in ${location}` : "";
+
+  return `${titlePrefix} ${titleSuffix}`;
+}
+
+// Inside the metadata object in the root layout.tsx, I have an option for the title to have a template as such: template: "%s | Flow Jobs"
+// The template only works for child pages. It does not work for the root page (this one). So I have to use a generateMetadata function here to generate the title metadata for the root page.
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: UrlProps): Metadata {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Flow Jobs`, // Have to hardcode "| Flow Jobs" here because the template in the root layout.tsx does not work for the root page.tsx
+  };
+}
+
+export default async function Home({
+  searchParams: { q, type, location, remote },
+}: UrlProps) {
+  const convertRemoteFromStringToBool: JobFilterType = {
+    q,
+    type,
+    location,
+    remote: remote === "true",
+  };
 
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
-        <div className="space-y-5 text-center">
-            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Developer Jobs
-            </h1>
+      <div className="space-y-5 text-center">
+        <H1>{getTitle(convertRemoteFromStringToBool)}</H1>
 
-            <p className="text-muted-foreground">
-                Find your dream job!
-            </p>
-        </div>
+        <p className="text-muted-foreground">Find your dream job!</p>
+      </div>
 
-        <section className="flex flex-col gap-4 md:flex-row">
-            <JobFilterSidebar />
-            
-            <div className="space-y-4 grow">
-                {jobsList.map((job) => (
-                <JobListItem key={job.id} job={job} />
-                ))}
-            </div>
-        </section>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar filterValues={convertRemoteFromStringToBool} />
+        <JobResults filterValues={convertRemoteFromStringToBool} />
+      </section>
     </main>
-  )
+  );
 }
---
+++ src/components/JobResults.tsx
@@ -0,0 +1,58 @@
+import { JobFilterType } from "@/lib/validations";
+import JobListItem from "./JobListItem";
+import prisma from "@/lib/prisma";
+import { Prisma } from "@prisma/client";
+
+type JobResultsProps = {
+  filterValues: JobFilterType;
+};
+
+export default async function JobResults({
+  filterValues: { q, type, location, remote },
+}: JobResultsProps) {
+  const searchString = q
+    ?.split(" ")
+    .filter((word) => word.length > 0)
+    .join(" & ");
+
+  const searchFilter: Prisma.JobWhereInput = searchString
+    ? {
+        OR: [
+          { title: { search: searchString } },
+          { companyName: { search: searchString } },
+          { type: { search: searchString } },
+          { locationType: { search: searchString } },
+          { location: { search: searchString } },
+        ],
+      }
+    : {};
+
+  const allJobFilters: Prisma.JobWhereInput = {
+    AND: [
+      searchFilter,
+      type ? { type } : {},
+      location ? { location } : {},
+      remote ? { locationType: "Remote" } : {},
+      { approved: true },
+    ],
+  };
+
+  const jobsList = await prisma.job.findMany({
+    where: allJobFilters,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div className="grow space-y-4">
+      {jobsList.map((job) => (
+        <JobListItem key={job.id} job={job} />
+      ))}
+
+      {jobsList.length === 0 && (
+        <p className="m-auto text-center">
+          No jobs found. Try adjusting your job filters.
+        </p>
+      )}
+    </div>
+  );
+}
